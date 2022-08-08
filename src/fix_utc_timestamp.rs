017bdba6dfddfd15b2de0052de767ce59c9ae8a1
Karuna Murti <karuna.murti@gmail.com>
2022-08-08 19:10:14 +0900

    fix_utc_timestamp

---
+++ src/fix_utc_timestamp.rs
@@ -0,0 +1,159 @@
+use crate::field::{FieldValue, FieldValueReader, FieldValueWriter};
+use chrono::naive::NaiveDateTime;
+
+#[derive(Default, Clone, Copy, Debug, PartialEq)]
+pub enum TimestampPrecision {
+    #[default]
+    Millis,
+    Seconds,
+    Micros,
+    Nanos,
+}
+
+const UTC_TIMESTAMP_SECONDS_FORMAT: &str = "%Y%m%d-%H:%M:%S";
+const UTC_TIMESTAMP_MILLIS_FORMAT: &str = "%Y%m%d-%H:%M:%S%.3f";
+const UTC_TIMESTAMP_MICROS_FORMAT: &str = "%Y%m%d-%H:%M:%S%.6f";
+const UTC_TIMESTAMP_NANOS_FORMAT: &str = "%Y%m%d-%H:%M:%S%.9f";
+
+// FIXUTCTimestamp is a FIX UTC Timestamp value, implements FieldValue
+#[derive(Default)]
+pub struct FIXUTCTimestamp {
+    pub time: NaiveDateTime,
+    pub precision: TimestampPrecision,
+}
+
+impl FieldValueReader for FIXUTCTimestamp {
+    fn read(&mut self, input: &str) -> Result<(), ()> {
+        match input.len() {
+            17 => {
+                self.precision = TimestampPrecision::Seconds;
+                self.time = NaiveDateTime::parse_from_str(input, UTC_TIMESTAMP_SECONDS_FORMAT)
+                    .map_err(|_| ())?;
+            }
+            21 => {
+                self.precision = TimestampPrecision::Millis;
+                self.time = NaiveDateTime::parse_from_str(input, UTC_TIMESTAMP_MILLIS_FORMAT)
+                    .map_err(|_| ())?;
+            }
+            24 => {
+                self.precision = TimestampPrecision::Micros;
+                self.time = NaiveDateTime::parse_from_str(input, UTC_TIMESTAMP_MICROS_FORMAT)
+                    .map_err(|_| ())?;
+            }
+            27 => {
+                self.precision = TimestampPrecision::Nanos;
+                self.time = NaiveDateTime::parse_from_str(input, UTC_TIMESTAMP_NANOS_FORMAT)
+                    .map_err(|_| ())?;
+            }
+            _ => (),
+        };
+
+        Ok(())
+    }
+}
+
+impl FieldValueWriter for FIXUTCTimestamp {
+    fn write(&self) -> String {
+        match self.precision {
+            TimestampPrecision::Seconds => {
+                self.time.format(UTC_TIMESTAMP_SECONDS_FORMAT).to_string()
+            }
+            TimestampPrecision::Millis => self.time.format(UTC_TIMESTAMP_MILLIS_FORMAT).to_string(),
+            TimestampPrecision::Micros => self.time.format(UTC_TIMESTAMP_MICROS_FORMAT).to_string(),
+            TimestampPrecision::Nanos => self.time.format(UTC_TIMESTAMP_NANOS_FORMAT).to_string(),
+        }
+    }
+}
+
+impl FieldValue for FIXUTCTimestamp {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use chrono::naive::NaiveDate;
+
+    #[test]
+    fn test_fixutc_timestamp_write() {
+        let ts = NaiveDate::from_ymd(2016, 2, 8).and_hms_nano(22, 7, 16, 954_123_123);
+
+        struct TestStruct {
+            precision: TimestampPrecision,
+            val: String,
+        }
+
+        let tests = vec![
+            TestStruct {
+                precision: TimestampPrecision::Millis,
+                val: "20160208-22:07:16.954".to_string(),
+            },
+            TestStruct {
+                precision: TimestampPrecision::Seconds,
+                val: "20160208-22:07:16".to_string(),
+            },
+            TestStruct {
+                precision: TimestampPrecision::Micros,
+                val: "20160208-22:07:16.954123".to_string(),
+            },
+            TestStruct {
+                precision: TimestampPrecision::Nanos,
+                val: "20160208-22:07:16.954123123".to_string(),
+            },
+        ];
+
+        for test in tests.iter() {
+            let mut f = FIXUTCTimestamp::default();
+            f.time = ts;
+            f.precision = test.precision;
+            let b = f.write();
+            assert_eq!(b, test.val, "got {}; want {}", b, test.val);
+        }
+    }
+
+    #[test]
+    fn test_fixutc_timestamp_read() {
+        struct TestStruct<'a> {
+            time_str: &'a str,
+            expected_time: NaiveDateTime,
+            expected_precision: TimestampPrecision,
+        }
+
+        let tests = vec![
+            TestStruct {
+                time_str: "20160208-22:07:16.310",
+                expected_time: NaiveDate::from_ymd(2016, 2, 8).and_hms_nano(22, 7, 16, 310_000_000),
+                expected_precision: TimestampPrecision::Millis,
+            },
+            TestStruct {
+                time_str: "20160208-22:07:16",
+                expected_time: NaiveDate::from_ymd(2016, 2, 8).and_hms_nano(22, 7, 16, 0),
+                expected_precision: TimestampPrecision::Seconds,
+            },
+            TestStruct {
+                time_str: "20160208-22:07:16.123455",
+                expected_time: NaiveDate::from_ymd(2016, 2, 8).and_hms_nano(22, 7, 16, 123_455_000),
+                expected_precision: TimestampPrecision::Micros,
+            },
+            TestStruct {
+                time_str: "20160208-22:07:16.954123123",
+                expected_time: NaiveDate::from_ymd(2016, 2, 8).and_hms_nano(22, 7, 16, 954_123_123),
+                expected_precision: TimestampPrecision::Nanos,
+            },
+        ];
+
+        for test in tests.iter() {
+            let mut f = FIXUTCTimestamp::default();
+            let result = f.read(test.time_str);
+            assert!(result.is_ok(), "Unexpected error: {:?}", result);
+            assert_eq!(
+                f.time, test.expected_time,
+                "For Time expected {} got {}",
+                test.expected_time, f.time
+            );
+            assert_eq!(
+                f.precision, test.expected_precision,
+                "For NoMillis expected expected {:?} got {:?}",
+                test.expected_precision, f.precision,
+            );
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -12,15 +12,16 @@ pub mod fix_decimal;
 pub mod fix_float;
 pub mod fix_int;
 pub mod fix_string;
+pub mod fix_utc_timestamp;
 pub mod log;
 pub mod session_id;
 pub mod tag;
 pub mod tag_value;
 
 //FIX BeginString string values
-const BEGIN_STRING_FIX40: &'static str = "FIX.4.0";
-const BEGIN_STRING_FIX41: &'static str = "FIX.4.1";
-const BEGIN_STRING_FIX42: &'static str = "FIX.4.2";
-const BEGIN_STRING_FIX43: &'static str = "FIX.4.3";
-const BEGIN_STRING_FIX44: &'static str = "FIX.4.4";
-const BEGIN_STRING_FIXT11: &'static str = "FIXT.1.1";
+const BEGIN_STRING_FIX40: &str = "FIX.4.0";
+const BEGIN_STRING_FIX41: &str = "FIX.4.1";
+const BEGIN_STRING_FIX42: &str = "FIX.4.2";
+const BEGIN_STRING_FIX43: &str = "FIX.4.3";
+const BEGIN_STRING_FIX44: &str = "FIX.4.4";
+const BEGIN_STRING_FIXT11: &str = "FIXT.1.1";
