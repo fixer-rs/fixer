7619472ac41d90f4164b17e85936bf3c347d917e
Karuna Murti <karuna.murti@gmail.com>
2022-09-04 23:23:12 +0900

    session_id

--- src/session_id.rs
+++ src/session_id.rs
@@ -1,4 +1,7 @@
+use std::string::ToString;
+
 // SessionID is a unique identifier of a Session
+#[derive(Debug, Default)]
 pub struct SessionID {
     pub begin_string: String,
     pub target_comp_id: String,
@@ -20,8 +23,92 @@ fn append_optional(buffer: &mut String, delim: &str, v: &str) {
 }
 
 impl SessionID {
-    // is_fixt returns true if the SessionID has a FIXT BeginString
+    // is_fixt returns true if the SessionID has a FIXT begin_string
     pub fn is_fixt(&self) -> bool {
         self.begin_string == crate::BEGIN_STRING_FIXT11
+    }
+}
+
+impl ToString for SessionID {
+    fn to_string(&self) -> String {
+        let mut result = self.begin_string.clone();
+        result.push_str(":");
+        result.push_str(&self.sender_comp_id);
+
+        append_optional(&mut result, "/", &self.sender_sub_id);
+        append_optional(&mut result, "/", &self.sender_location_id);
+
+        result.push_str("->");
+        result.push_str(&self.target_comp_id);
+
+        append_optional(&mut result, "/", &self.target_sub_id);
+        append_optional(&mut result, "/", &self.target_location_id);
+
+        append_optional(&mut result, ":", &self.qualifier);
+        result
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_session_id_string() {
+        struct TestCase {
+            session_id: SessionID,
+            expected_string: String,
+        }
+        let tests = vec![
+            TestCase {
+                session_id: SessionID {
+                    begin_string: String::from("FIX.4.2"),
+                    sender_comp_id: String::from("SND"),
+                    target_comp_id: String::from("TAR"),
+                    ..Default::default()
+                },
+                expected_string: String::from("FIX.4.2:SND->TAR"),
+            },
+            TestCase {
+                session_id: SessionID {
+                    begin_string: String::from("FIX.4.2"),
+                    sender_comp_id: String::from("SND"),
+                    target_comp_id: String::from("TAR"),
+                    qualifier: String::from("BLAH"),
+                    ..Default::default()
+                },
+                expected_string: String::from("FIX.4.2:SND->TAR:BLAH"),
+            },
+            TestCase {
+                session_id: SessionID {
+                    begin_string: String::from("FIX.4.2"),
+                    sender_comp_id: String::from("SND"),
+                    sender_sub_id: String::from("SSUB"),
+                    sender_location_id: String::from("SLOC"),
+                    target_comp_id: String::from("TAR"),
+                    target_sub_id: String::from("TSUB"),
+                    target_location_id: String::from("TLOC"),
+                    qualifier: String::from("BLAH"),
+                },
+                expected_string: String::from("FIX.4.2:SND/SSUB/SLOC->TAR/TSUB/TLOC:BLAH"),
+            },
+            TestCase {
+                session_id: SessionID {
+                    begin_string: String::from("FIX.4.2"),
+                    sender_comp_id: String::from("SND"),
+                    sender_location_id: String::from("SLOC"),
+                    target_comp_id: String::from("TAR"),
+                    target_sub_id: String::from("TSUB"),
+                    target_location_id: String::from("TLOC"),
+                    ..Default::default()
+                },
+                expected_string: String::from("FIX.4.2:SND/SLOC->TAR/TSUB/TLOC"),
+            },
+        ];
+
+        for test in tests.iter() {
+            let actual = test.session_id.to_string();
+            assert_eq!(test.expected_string, actual);
+        }
     }
 }
