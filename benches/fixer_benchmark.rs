01236e77e3bffd7fe2c7f162b768b590979f307b
Karuna Murti <karuna.murti@gmail.com>
2023-08-28 16:05:21 +0900

    add flamegraph to bench

--- benches/fixer_benchmark.rs
+++ benches/fixer_benchmark.rs
@@ -1,17 +1,19 @@
-use criterion::{criterion_group, criterion_main};
+use criterion::{criterion_group, criterion_main, Criterion};
+use pprof::{
+    criterion::{Output, PProfProfiler},
+    flamegraph::Options,
+};
 
-pub mod fix_float;
-pub mod fix_int;
-pub mod internal;
-pub mod message;
-pub mod parser;
+pub(crate) mod fix_float;
+pub(crate) mod fix_int;
+pub(crate) mod internal;
+pub(crate) mod message;
+pub(crate) mod parser;
 
-criterion_group!(
-    benches,
-    fix_float::benchmark_fix_float_read,
-    fix_int::benchmark_fix_int_read,
-    internal::time_range::benchmark_in_range,
-    parser::benchmark_parser_read_message,
-    message::parse_message,
-);
+criterion_group! {
+    name = benches;
+    config = Criterion::default().with_profiler(PProfProfiler::new(100, Output::Flamegraph(Some(Options::default()))));
+    targets = fix_float::benchmark_fix_float_read, fix_int::benchmark_fix_int_read, internal::time_range::benchmark_in_range, parser::benchmark_parser_read_message,message::parse_message
+}
+
 criterion_main!(benches);
